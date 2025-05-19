78cf4556867e63928aee1d066a85e2e732f89fa2
unknown <saivishwaram.ramkumar@gmail.com>
2025-05-19 19:57:11 +0530

    A Card component and glb is added needed to be modified as a bulb for swiching themes

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Lanyard from './components/Bulb/Bulb'
 
 function App() {
 
   return (
     <div className='w-screen h-screen bg-red-800 text-white text-6xl flex justify-center items-center'>
         Hey this is my portfolio website
+        <Lanyard />
     </div>
   )
 }
